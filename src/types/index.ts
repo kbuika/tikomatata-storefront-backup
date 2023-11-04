a0a750e40312baf381e87437e4dd813e9bb3a32f
kibuikaCodes <kibuikasteve@gmail.com>
2023-11-04 12:28:22 +0300

    feat: only initialize payment after ticket reservation

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,7 +6,12 @@ export interface PaystackHookType {
     onSuccess: callback;
     onClose: () => void;
     config: any;
-    payForTickets: () => void;
+    payForTickets?: () => void;
     validateForm: () => true | false;
     paymentMethod: PaymentChannels;
+    paymentReference: string;
+    handleSubmit: (e: any) => void;
+    initialized: boolean;
+    setInitialized: (value: boolean) => void;
+
 }
