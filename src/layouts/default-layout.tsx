075e52ce8ba4b480aac983018d95d831d1883c17
kibuikaCodes <kibuikasteve@gmail.com>
2023-11-23 10:58:53 +0300

    add dynamic link on layout

--- src/layouts/default-layout.tsx
+++ src/layouts/default-layout.tsx
@@ -38,7 +38,11 @@ const DefaultLayout: React.FC<Props> = ({
         </Link>
         <div className="md:flex items-center justify-between hidden space-x-8">
           <Link
-            href={process.env.ENV == "production" ? "https://app.tikomatata.com" : "https://dev-app.tikomatata.com"}
+            href={
+              process.env.ENV == "production"
+                ? "https://app.tikomatata.com"
+                : "https://dev-app.tikomatata.com"
+            }
             target="_blank"
             rel="noopener noreferrer"
             className=" text-gray-400 hover:text-white"
@@ -48,22 +52,45 @@ const DefaultLayout: React.FC<Props> = ({
         </div>
       </div>
       <main>{children}</main>
-      <footer className={`footer-container h-[40vh] w-full flex items-center justify-center min-[768px]:hidden  ${noFooter ? "hidden" : ""}`}>
+      <footer
+        className={`footer-container h-[40vh] w-full flex items-center justify-center min-[768px]:hidden  ${
+          noFooter ? "hidden" : ""
+        }`}
+      >
         <div className="main-footer-div p-8 w-full h-full text-dark flex flex-col items-center justify-center md:w-[50%]">
           <div className="w-full flex flex-row items-start justify-between mb-4">
             <div>
               <h2 className="font-bold text-xl">Resources</h2>
-              <p className="mt-4 text-lg">Terms</p>
+              <p className="mt-4 text-lg">
+                <Link
+                  href="https://docs.google.com/document/d/1IyzMAE7d-XDVShFPel1-CuDjXvZ32hEJ8dBPmWqVj58/edit?usp=sharing
+"
+                  className="hover:underline"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Terms
+                </Link>
+              </p>
               <p className="mt-4 text-lg">Privacy</p>
-              <p className="mt-4 text-lg">Talk to Us</p>
-              <p className="mt-4 text-lg">Have an issue? Contact Us</p>
+              <p className="mt-4 text-lg">
+                <Link href="/contact" className="hover:underline">
+                  Contact Us
+                </Link>
+              </p>
             </div>
             <div>
               <h2 className="font-bold text-xl">Socials</h2>
-              <p className="mt-4 text-lg">Instagram</p>
-              <p className="mt-4 text-lg">X</p>
-              <p className="mt-4 text-lg">LinkedIn</p>
-              <p className="mt-4 text-lg">Facebook</p>
+              <p className="mt-4 text-lg">
+                <Link
+                  href="https://www.instagram.com/tikomatata.ke/"
+                  className="hover:underline"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Instagram
+                </Link>
+              </p>
             </div>
           </div>
           <div className=" mt-8 flex flex-row w-full items-center justify-between">
