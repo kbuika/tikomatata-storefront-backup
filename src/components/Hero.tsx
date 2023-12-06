a36372df569efc2068829b6f34b1dd1d7b79d55a
kibuikaCodes <kibuikasteve@gmail.com>
2023-12-06 21:18:33 +0300

    add image credits

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,9 @@
-"use client"
 import React from "react"
 
 const Hero = () => {
   return (
-    <div className="w-full h-full md:px-[80px] px-[20px] flex flex-col md:items-center justify-center">
-      <h1 className="hero-text text-white mb-24">
+    <div className="w-full h-[80%] md:px-[80px] px-[20px] flex flex-col md:items-center justify-center">
+      <h1 className="hero-text text-white mb-18">
         Live it up!
       </h1>
       {/* <p className='mb-2 md:text-center md:text-lg md:text-gray-100 text-white'>
