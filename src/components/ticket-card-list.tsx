e0bb5b145f5005b68c7b549459ebd4e6c3941959
kibuikaCodes <kibuikasteve@gmail.com>
2023-12-31 00:58:19 +0300

    highlight gate tickets

--- src/components/ticket-card-list.tsx
+++ src/components/ticket-card-list.tsx
@@ -8,13 +8,12 @@ interface TicketCardListProps {
 
 const TicketCardList: React.FC<TicketCardListProps> = ({ tickets, event }) => {
   if (!Array.isArray(tickets)) {
-    console.error("Invalid tickets data. Expected an array.")
     return null
   }
   const sortedTickets = [...tickets]?.sort((a, b) => {
     // "Flash Sale" should come first, so we give it a lower sort value
-    if (a.name === "Flash Sale") return -1
-    if (b.name === "Flash Sale") return 1
+    if (a.name === "Gate Regular Ticket") return -1
+    if (b.name === "Gate Regular Ticket") return 1
 
     // For other tickets, use default alphabetical sorting
     return a.name.localeCompare(b.name)
